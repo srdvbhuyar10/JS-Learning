e9ddd13aa8b8fbbae168b2590f717e7b0eefca92
srdvbhuyar10 <srdvbhuyar10@gmail.com>
2023-07-19 07:50:02 +0530

    js programming

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+
+var myName; // variable declaration
+myName ="Sridevi"; // variable declaration or Initialization
+
+console.log(myName);
+
+var city ="Pune"; // variable declaration or Initialization
+
+console.log(city);
+
+var pin; //// variable declaration
+console.log(pin); 
