bf68b3176feee0628966426537f121c87ec98039
srdvbhuyar10 <srdvbhuyar10@gmail.com>
2023-07-31 23:01:07 +0530

    for and while loop committed and pushed

---
+++ .vscode/06_for_Assignment01.js
@@ -0,0 +1,43 @@
+
+
+console.log(`---------------------------`);
+// Log numbers from 5 to 15
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+}
+
+console.log("----------------------------")
+// decrementing from 50 to 40
+for (let index = 50; index >=40 ; index=index-1) {
+    console.log(index);  
+}
+
+console.log("----------------------------")
+// First 15 odd numbers
+for (let index = 1; index <15 ; index=index+2) {
+    console.log(index); 
+}
+
+console.log("----------------------------")
+// First 10 even number
+for (let index = 0; index <20 ; index=index+2) {
+    console.log(index); 
+}
+
+console.log("----------------------------")
+// 5 table
+for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index);  
+}
+
+console.log("----------------------------")
+// 10 table
+for (let index = 10; index <=100 ; index=index+10) {
+    console.log(index);  
+}
+
+console.log("----------------------------")
+// 10 table in reverse order
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index);  
+}
---
+++ .vscode/06_while_Assignment01.js
@@ -0,0 +1,57 @@
+
+
+console.log(`---------------------`);
+// Numbers from 5 to 15
+var i = 5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+
+console.log(`---------------------`);
+// Decrementing by 1 from 50 to 40 
+var i = 50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+
+console.log(`---------------------`);
+// first 15 odd numbers 
+var i = 1;
+while (i<=29) {
+    console.log(i);
+    i+=2;
+}
+
+console.log(`---------------------`);
+// first 10 even numbers 
+var i = 0;
+while (i<=18) {
+    console.log(i);
+    i+=2;
+}
+
+console.log(`---------------------`);
+// print table of 5
+var i = 5;
+while (i<=50) {
+    console.log(i);
+    i+=5;
+}
+
+console.log(`---------------------`);
+// print table of 10
+var i = 10;
+while (i<=100) {
+    console.log(i);
+    i+=10;
+}
+
+console.log(`---------------------`);
+// print table of 10 in Reverse order
+var i = 100 ;
+while (i>=10) {
+    console.log(i);
+    i-=10;
+}
